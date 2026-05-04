47a2644cd90ef4066fd9be77a85b8827248bcf95
Anish Shrestha <108356769+anish-X@users.noreply.github.com>
2026-05-04 09:28:05 +0545

    Update notesController.js

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -6,7 +6,7 @@ export const getAllNotes = async (req, res) => {
     res.status(200).json(notes);
   } catch (error) {
     console.error("Error fetching notes:", error);
-    res.status(500).json({ error: "dsds sdfsdfs fetch notes" });
+    res.status(500).json({ error: "dsds sdfs fetch notes" });
   }
 };
 
