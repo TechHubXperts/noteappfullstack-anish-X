cc28231050b11eda9dc103f1c1477655fabc43bb
Anish Shrestha <108356769+anish-X@users.noreply.github.com>
2026-05-04 09:11:43 +0545

    Update notesController.js

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -6,7 +6,7 @@ export const getAllNotes = async (req, res) => {
     res.status(200).json(notes);
   } catch (error) {
     console.error("Error fetching notes:", error);
-    res.status(500).json({ error: "fasdfasdfsa fetch notes" });
+    res.status(500).json({ error: "asdfasdfasdf fetch notes" });
   }
 };
 
